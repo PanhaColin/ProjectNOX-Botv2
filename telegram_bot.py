78058ea043fef1ed1ae6d56505274e750c5422df
Meyson Sopanha <89469514+PanhaColin@users.noreply.github.com>
2024-09-19 18:15:35 +0000

    commit

--- telegram_bot.py
+++ telegram_bot.py
@@ -103,7 +103,7 @@ async def total_price(update: Update, context: CallbackContext) -> int:
 async def get_topic_id(update: Update, context: CallbackContext) -> None:
     topic_id = update.effective_chat.id
     # Send the topic ID to Make.com via webhook
-    make_url = 'https://hook.us2.make.com/vi87j2q29haw6ocfxknjxwl5fecktmk9'  # Replace with your actual webhook URL
+    make_url = 'https://hook.us2.make.com/a8x90abvt3nijoi7gydmplwwt273ex8h'  # Replace with your actual webhook URL
     requests.post(make_url, json={"topic_id": topic_id})
 
 # Callback handler when "Send Receipt" button is clicked
